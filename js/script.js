2b929e9413f3987daf69749c9e5a88e0abfe65cd
Marc Proux <prouxmarc@gmail.com>
2023-02-01 14:27:27 +0100

    ajout page de profil

--- js/script.js
+++ js/script.js
@@ -1,10 +1,51 @@
 const headerEl = $('.header');
-
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 500) {
         $('.header').addClass('header-scrolled');
     }
     else {
         $('.header').removeClass('header-scrolled');
     }
+    navHighlighter();
 });
+
+$(function() {
+    AOS.init();
+    $("#home-sec").addClass("active");
+});
+
+
+$(function() {
+    var typed = $('.typed');
+    typed.each(function() {
+        var typed = new Typed(this, {
+            strings: ["Informatique", "Réseaux", "Développement Web"],
+            typeSpeed: 50,
+            loop: true,
+            backDelay:1500,
+            backSpeed: 30,
+            showCursor: false,
+        });
+    });
+});
+
+function navHighlighter() {
+    var sections = $("section[id]");
+    let scrollY = $(window).scrollTop();
+    sections.each(function() {
+        var current = $(this);
+        var sectionHeight = current.height();
+        var sectionTop = current.offset().top - 50;
+        var sectionId = current.attr("id");
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            $("#"+sectionId + "-sec").addClass("active");
+        } else {
+            $("#"+sectionId + "-sec").removeClass("active");
+        }
+    }
+    );
+}
+
+var scrollFunction = function(id) {
+    window.scrollTo(0, $("#" + id).offset().top);
+}
