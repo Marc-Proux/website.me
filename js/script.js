424ed0723d8cf20ca0900b06dabeda3f4572a07a
Marc Proux <prouxmarc@gmail.com>
2023-02-04 01:29:02 +0100

    debut page skills

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const headerEl = $('.header');
+var currentSlide = 0;
+var skillShow = false;
+
 window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
         $('.header').addClass('header-scrolled');
@@ -12,6 +15,7 @@ window.addEventListener('scroll', () => {
 $(function() {
     AOS.init();
     $("#home-sec").addClass("active");
+    showSlide(currentSlide);
 });
 
 
@@ -49,3 +53,65 @@ function navHighlighter() {
 var scrollFunction = function(id) {
     window.scrollTo(0, $("#" + id).offset().top);
 }
+
+function changeSlide(n) {
+    showSlide(currentSlide + n);
+}
+
+function showSlide(index){
+    var slides = $(".slides");
+    var dots = $(".dot");
+    if (index >= slides.length) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    for (var i = 0; i < index; i++) {
+        element = $("#slide-" + i);
+        $(element).css('left', (-(100*(index-i)+10))+'%');
+    }
+    for (var i = index; i < slides.length; i++) {
+        element = $("#slide-" + i);
+        $(element).css('left', (-(100*(index-i))+10)+'%');
+    }
+    $(dots[currentSlide]).removeClass("active");
+    currentSlide = index;
+    animateProgressBar("slide-"+currentSlide);
+    $(dots[currentSlide]).addClass("active");
+
+}
+
+function animateProgressBar(slide){
+    var progressCircle = $("#"+slide+" .progress-bar--inner svg circle");
+    const svgStrokeDashArray = parseInt(window.getComputedStyle(progressCircle[0]).getPropertyValue("stroke-dasharray").replace("px", ""));
+    (progressCircle).each(function() {
+        var current = $(this);
+        var value = parseInt(current.attr("value"));
+        var offsetValue = Math.floor(((100 - value) * svgStrokeDashArray) / 100);
+        current.css({
+            'stroke-dashoffset': svgStrokeDashArray,
+        });
+        current.animate(
+            {strokeDashoffset: offsetValue},1000);
+        current.css("stroke-dashoffset", offsetValue);
+    });
+}
+
+window.onscroll = function(e) {
+    var section = $("#skills")
+    var sectionTop = section.offset().top-335;
+    if (!(this.oldScroll > this.scrollY)) {
+        console.log(this.scrollY);
+        console.log(skillShow)
+        if (this.scrollY > sectionTop && skillShow == false) {
+            animateProgressBar("slide-"+currentSlide);
+            skillShow = true;
+        }
+    }
+    else if (this.oldScroll > this.scrollY && this.scrollY < sectionTop) {
+        skillShow = false;
+    }
+    this.oldScroll = this.scrollY;
+}
+  
